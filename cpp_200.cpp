c9b16fe3fb4bde94ee12c3b82a4b76db7e476db5
Jason-time <noconal+jason-time-github@gmail.com>
2020-07-25 23:43:28 +0900

    // 044 - 실수 반올림하기(round)

--- cpp_200.cpp
+++ cpp_200.cpp
@@ -3,21 +3,12 @@
 using namespace std;
 
 int main(int argc, char* argv[]) {
-	cout << "== 소수점 버리기 ==" << endl;
-	cout << "floor(1.1) : " << floor(1.1) << endl;
-	cout << "floor(-2.3) : " << floor(-2.3) << endl;
-	cout << "floor(40.5) : " << floor(40.5) << endl;
-	cout << "floor(-55.7) : " << floor(-55.7) << endl;
-	cout << "floor(100.9) : " << floor(100.9) << endl;
-	
-	cout << "== 소수점 올리기 ==" << endl;
-	cout << "ceil(1.1) : " << ceil(1.1) << endl;
-	cout << "ceil(-2.3) : " << ceil(-2.3) << endl;
-	cout << "ceil(40.5) : " << ceil(40.5) << endl;
-	cout << "ceil(-55.7) : " << ceil(-55.7) << endl;
-	cout << "ceil(100.9) : " << ceil(100.9) << endl;
-
+	cout << "round(1.1) : " << round(1.1) << endl;
+	cout << "round(-2.3) : " << round(-2.3) << endl;
+	cout << "round(40.5) : " << round(40.5) << endl;
+	cout << "round(-55.7) : " << round(-55.7) << endl;
+	cout << "round(100.9) : " << round(100.9) << endl;
 	return 0;
 }
 
-// 043 - 실수 소수점 버리기 올리기(floor, ceil)
+// 044 - 실수 반올림하기(round)
