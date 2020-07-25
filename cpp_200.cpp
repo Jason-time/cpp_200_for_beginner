3cb0aca9c61ef8aa77c93799047d1676a1df0b35
Jason-time <noconal+jason-time-github@gmail.com>
2020-07-25 21:46:13 +0900

    // 039 - 조건 순환문 이해하기2.(do~while~continue~break)

--- cpp_200.cpp
+++ cpp_200.cpp
@@ -4,16 +4,14 @@ using namespace std;
 
 int main(int argc, char* argv[]) {
 	int number = 0;
-
-	while (number <10)
-	{
+	do {
 		number++;
 		if (number % 3 == 0)
 			continue;
 		else
 			cout << "number 값 : " << number << endl;
-	}
+	} while (number < 10);
 	return 0;
 }
 
-// 038 - 조건 순환문 이해하기1.(while~continue~break)
+// 039 - 조건 순환문 이해하기2.(do~while~continue~break)
