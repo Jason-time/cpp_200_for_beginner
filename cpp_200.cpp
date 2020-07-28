8e0a0e0ec083756497d40bf61e88d33156242d47
Jason-time <noconal+jason-time-github@gmail.com>
2020-07-29 01:29:23 +0900

    // 056 - 문자열 대소문자 변환하기(toupper, tolower)

--- cpp_200.cpp
+++ cpp_200.cpp
@@ -1,15 +1,27 @@
 ﻿#include <iostream>
 #include<string>
+#include<algorithm>
 
 using namespace std; 
 int main(int argc, char* argv[]) {
-	string jinduk_name = "김승만";
-	string jinsung_name = "kim man";
+	string silla = "divided into the Three Kingdoms.";
+	string joseon = "Yi Seong-gye, established Joseon in 1392";
 
-	cout << "진덕여왕 이름 길이 : " << jinduk_name.length() << endl;
-	cout << "진성여왕 이름 길이 : " << jinsung_name.length() << endl;
+	transform(silla.begin(), silla.end(), silla.begin(), toupper);
+	transform(joseon.begin(), joseon.end(), joseon.begin(), tolower);
+
+	char lower_ch = 'g';
+	char upper_ch = 'B';
+
+	lower_ch = toupper(lower_ch);
+	upper_ch = tolower(upper_ch);
+
+	cout << "문자열 대문자로 변환 : " << silla << endl;
+	cout << "문자열 소문자로 변환 : " << joseon<< endl;
+	cout << "문자 대문자로 변환 : " << lower_ch<< endl;
+	cout << "문자 소문자로 변환 : " << upper_ch<< endl;
 
 	return 0;
 }
 
-// 055 - 문자열 길이 구하기(length)
+// 056 - 문자열 대소문자 변환하기(toupper, tolower)
