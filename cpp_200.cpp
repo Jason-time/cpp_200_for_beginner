ba4fd5679fa6c95fb901d08f47dbe8901d4bbd48
Jason-time <noconal+jason-time-github@gmail.com>
2020-07-29 00:53:55 +0900

    // 052 - 지나간 시간 알아내기(time)

--- cpp_200.cpp
+++ cpp_200.cpp
@@ -1,20 +1,22 @@
-﻿#include <iostream>
-#define _CRT_SECURE_NO_WARNINGS
-#include <ctime>
+﻿#include <ctime>
+#include <iostream>
 
-using namespace std;
-
+using namespace std; 
 int main(int argc, char* argv[]) {
-	time_t now = time(NULL);
-	tm* ptm = localtime(&now);
+	time_t start = time(NULL);
+	time_t finish = time(NULL);
 
-	char buffer[64];
-	strftime(buffer, 64, "예제 만드는 지금은 %Y년 %m월 %d일, %H시 %M분 %S초 입니다.(%p)\n", ptm);
+	int pass_int = 2;
 
-	cout << buffer;
+	time(&start);
+	for (int i = 1;i < 10000;i++) {
+		for (int j = 1; j < 10000;j++)
+			pass_int += 1;
+	}
 
+	time(&finish);
+	cout << "1을 100억 번 더하는 시간 : " << difftime(finish, start) << "초 \n";
 	return 0;
-
 }
 
-// 051 - 날짜와 시간을 문자열로 변환하기(localtime) 실행 이 안 된 다. 
+// 052 - 지나간 시간 알아내기(time)
