926e72241766fdf15e61b67f22b62ea804d98b34
Jason-time <noconal+jason-time-github@gmail.com>
2020-08-08 17:03:58 +0900

    // 062 - 문자열 일부 교체하기(replace)

--- cpp_200.cpp
+++ cpp_200.cpp
@@ -5,10 +5,13 @@ using namespace std;
 
 int main(int argc, char* argv[]) {
 	string sentence = "I like coding";
-	sentence.erase(remove(sentence.begin(), sentence.end(), ' '),sentence.end());
+	string find_str = "coding";
+	string replace_str = "history";
+
+	sentence.replace(sentence.find(find_str), find_str.length(), replace_str);
 
 	cout << sentence << endl;
 	return 0;
 }
 
-// 061 - 문자열에서 특정 문자만 제거하기(erase, remove)
+// 062 - 문자열 일부 교체하기(replace)
